31f23ad59016d9330f4d53581e7d4178bd620b9e
everpcpc <git@everpcpc.com>
2024-04-20 00:47:57 +0800

    feat: add oauth sign start

--- Bangumi/AuthView.swift
+++ Bangumi/AuthView.swift
@@ -5,18 +5,30 @@
 //  Created by Chuan Chuan on 2024/4/19.
 //
 
+import AuthenticationServices
 import SwiftUI
+import UIKit
 
 struct AuthView: View {
-    @EnvironmentObject var errorHandling: ErrorHandling
+    @StateObject var viewModel = SignInViewModel()
 
     var body: some View {
-        Button(action: login) {
-            Label("Login", systemImage: "person")
+        Button {
+            viewModel.signIn()
+        } label: {
+            Text("Sign in with Bangumi")
         }.buttonStyle(.borderedProminent)
     }
+}
 
-    private func login() {
+class SignInViewModel: NSObject, ObservableObject, ASWebAuthenticationPresentationContextProviding {
+    @EnvironmentObject var errorHandling: ErrorHandling
+
+    func presentationAnchor(for session: ASWebAuthenticationSession) -> ASPresentationAnchor {
+        return ASPresentationAnchor()
+    }
+
+    func signIn() {
         guard let infoDictionary = Bundle.main.infoDictionary else {
             errorHandling.handle(message: "infoDictionary is nil")
             return
@@ -29,7 +41,35 @@ struct AuthView: View {
             errorHandling.handle(message: "BANGUMI_APP_SECRET is nil")
             return
         }
-        print(appID, appSecret)
+
+        let scheme = "bangumi"
+        guard let baseURL = URL(string: "https://bgm.tv/oauth/authorize") else { return }
+        let qAppID = URLQueryItem(name: "client_id", value: appID)
+        let qResponseType = URLQueryItem(name: "response_type", value: "code")
+        let qRedirectURI = URLQueryItem(name: "redirect_uri", value: scheme + "://oauth/callback")
+        let authURL = baseURL.appending(queryItems: [qAppID, qResponseType, qRedirectURI])
+
+        let session = ASWebAuthenticationSession(url: authURL, callbackURLScheme: scheme) { (callback: URL?, error: Error?) in
+            guard error == nil, let successURL = callback else {
+                return
+            }
+            let query = URLComponents(string: successURL.absoluteString)?
+                .queryItems?.filter { $0.name == "code" }.first
+            let authorizationCode = query?.value ?? ""
+            print(authorizationCode)
+            // Have to wrap the code in a Task block because
+            // ASWebAuthenticationSession does not
+            // support Swift concurrency (async await).
+            Task {
+//              if let token = await self.exchangeAuthorizationCodeFor
+//              AccessToken(code: authorizationCode) {
+//                  self.saveTokenInKeychain(token: token)
+//              }
+            }
+        }
+        session.presentationContextProvider = self
+        session.prefersEphemeralWebBrowserSession = false
+        session.start()
     }
 }
 
