ad06351b3668e0c9757692ed9f448dce004615c7
everpcpc <git@everpcpc.com>
2024-04-21 08:27:18 +0800

    feat: auto refresh token

--- Bangumi/Models/Chii.swift
+++ Bangumi/Models/Chii.swift
@@ -22,6 +22,7 @@ class ChiiAPI: ObservableObject, Observable {
     let auth: Auth
 
     let apiBase = URL(string: "https://api.bgm.tv")!
+    let userAgent = "everpcpc/Bangumi/0.0.1 (iOS)"
     var session: URLSession
 
     init(errorHandling: ErrorHandling, modelContext: ModelContext, auth: Auth) {
@@ -31,13 +32,54 @@ class ChiiAPI: ObservableObject, Observable {
 
         let sessionConfig = URLSessionConfiguration.default
         sessionConfig.httpAdditionalHeaders = [
-            "User-Agent": "everpcpc/Bangumi/0.0.1 (iOS)",
+            "User-Agent": self.userAgent,
             "Authorization": "Bearer \(auth.accessToken)"
         ]
         self.session = URLSession(configuration: sessionConfig)
     }
 
+    func checkRefreshAccessToken() async throws {
+        if !self.auth.isExpired() {
+            return
+        }
+        guard let plist = Bundle.main.infoDictionary else {
+            throw ChiiError(message: "Could not find Info.plist")
+        }
+        guard let clientID = plist["BANGUMI_APP_ID"] as? String else {
+            throw ChiiError(message: "Could not find BANGUMI_APP_ID in Info.plist")
+        }
+        guard let clientSecret = plist["BANGUMI_APP_SECRET"] as? String else {
+            throw ChiiError(message: "Could not find BANGUMI_APP_SECRET in Info.plist")
+        }
+        var request = URLRequest(url: URL(string: "https://bgm.tv/oauth/access_token")!)
+        request.httpMethod = "POST"
+        let body = [
+            "grant_type": "refresh_token",
+            "client_id": clientID,
+            "client_secret": clientSecret,
+            "refresh_token": self.auth.refreshToken,
+            "redirect_uri": "bangumi://oauth/callback"
+        ]
+        let bodyData = try? JSONSerialization.data(withJSONObject: body)
+        request.httpBody = bodyData
+        let (data, response) = try await URLSession.shared.data(for: request)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else {
+            throw ChiiError(message: "failed to refresh access token")
+        }
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let resp = try decoder.decode(TokenResponse.self, from: data)
+        self.auth.update(response: resp)
+        let sessionConfig = URLSessionConfiguration.default
+        sessionConfig.httpAdditionalHeaders = [
+            "User-Agent": self.userAgent,
+            "Authorization": "Bearer \(self.auth.accessToken)"
+        ]
+        self.session = URLSession(configuration: sessionConfig)
+    }
+
     func get(url: URL) async throws -> Data {
+        try await self.checkRefreshAccessToken()
         var request = URLRequest(url: url)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "GET"
@@ -49,6 +91,7 @@ class ChiiAPI: ObservableObject, Observable {
     }
 
     func post(url: URL, body: Data) async throws -> Data {
+        try await self.checkRefreshAccessToken()
         var request = URLRequest(url: url)
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
         request.httpMethod = "POST"
@@ -62,16 +105,16 @@ class ChiiAPI: ObservableObject, Observable {
     }
 
     func updateProfile() {
-        let url = apiBase.appendingPathComponent("v0/me")
+        let url = self.apiBase.appendingPathComponent("v0/me")
         Task { @MainActor in
             if let data = try? await get(url: url) {
                 let decoder = JSONDecoder()
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
                 let response = try decoder.decode(ProfileResponse.self, from: data)
                 let me = Profile(response: response)
-                modelContext.insert(me)
+                self.modelContext.insert(me)
             } else {
-                errorHandling.handle(message: "failed to get profile")
+                self.errorHandling.handle(message: "failed to get profile")
             }
         }
     }
@@ -142,6 +185,16 @@ final class Auth {
     }
 
     init(response: TokenResponse) {
+        self.accessToken = response.accessToken
+        self.expiresAt = Date().addingTimeInterval(TimeInterval(response.expiresIn))
+        self.refreshToken = response.refreshToken
+    }
+
+    func isExpired() -> Bool {
+        return Date() > self.expiresAt
+    }
+
+    func update(response: TokenResponse) {
         self.accessToken = response.accessToken
         self.expiresAt = Date().addingTimeInterval(TimeInterval(response.expiresIn))
         self.refreshToken = response.refreshToken
