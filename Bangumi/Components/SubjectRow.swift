1d0b49a2e04746b9ba137c59d9f7d9693b54c799
everpcpc <git@everpcpc.com>
2024-04-26 10:05:10 +0800

    fix: background for search row

--- Bangumi/Components/SubjectRow.swift
+++ Bangumi/Components/SubjectRow.swift
@@ -12,24 +12,32 @@ struct SubjectRow: View {
 
     var body: some View {
         let iconURL = URL(string: subject.images.common)
-        HStack {
-            CachedAsyncImage(url: iconURL) { image in
-                image.resizable().scaledToFill().frame(width: 64, height: 64).clipped()
-            } placeholder: {
-                Rectangle().fill(.accent.opacity(0.1)).frame(width: 64, height: 64)
+        ZStack {
+            Rectangle()
+                .fill(.accent)
+                .opacity(0.01)
+                .frame(height: 64)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .shadow(color: .accent, radius: 1, x: 1, y: 1)
+            HStack {
+                CachedAsyncImage(url: iconURL) { image in
+                    image.resizable().scaledToFill().frame(width: 64, height: 64).clipped()
+                } placeholder: {
+                    Rectangle().fill(.accent.opacity(0.1)).frame(width: 64, height: 64)
+                }
+                VStack(alignment: .leading) {
+                    let score = String(format: "%.1f", subject.score)
+                    Text(subject.name).font(.headline)
+                    Text(subject.nameCn).font(.subheadline).foregroundStyle(.gray)
+                    HStack {
+                        Label(subject.type.description, systemImage: subject.type.icon).foregroundStyle(.accent)
+                        Text("\(subject.collectionTotal) 人收藏").foregroundStyle(.gray)
+                        Spacer()
+                        Label("\(score)", systemImage: "star").foregroundStyle(.gray)
+                    }.font(.caption)
+                }
+                Spacer()
             }
-            VStack(alignment: .leading) {
-                let score = String(format: "%.1f", subject.score)
-                Text(subject.name).font(.headline)
-                Text(subject.nameCn).font(.subheadline).foregroundStyle(.gray)
-                HStack {
-                    Label(subject.type.description, systemImage: subject.type.icon).foregroundStyle(.accent)
-                    Text("\(subject.collectionTotal) 人收藏").foregroundStyle(.gray)
-                    Spacer()
-                    Label("\(score)", systemImage: "star").foregroundStyle(.gray)
-                }.font(.caption)
-            }
-            Spacer()
         }
         .frame(height: 64)
         .clipShape(RoundedRectangle(cornerRadius: 10))
