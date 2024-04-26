36fbaba1d0d19f1930aa75cc1a844f6843c49757
everpcpc <git@everpcpc.com>
2024-04-26 09:00:16 +0800

    chore: rename model files

--- Bangumi/Models/Chii.swift
+++ Bangumi/Models/Chii.swift
@@ -2,180 +2,200 @@
 //  Chii.swift
 //  Bangumi
 //
-//  Created by Chuan Chuan on 2024/4/19.
+//  Created by Chuan Chuan on 2024/4/21.
 //
 
-import Foundation
-import SwiftData
-
-struct ChiiError: Error {
-    var message: String
-
-    init(message: String) {
-        self.message = message
-    }
-}
-
-@Model
-final class Profile: Codable {
-    enum CodingKeys: String, CodingKey {
-        case id
-        case username
-        case nickname
-        case userGroup
-        case avatar
-        case sign
-    }
-
-    @Attribute(.unique)
-    var id: UInt
-    var username: String
-    var nickname: String
-    var userGroup: UInt
-    var avatar: Avatar
-    var sign: String
-
-    init(id: UInt, username: String, nickname: String, userGroup: UInt, avatar: Avatar, sign: String) {
-        self.id = id
-        self.username = username
-        self.nickname = nickname
-        self.userGroup = userGroup
-        self.avatar = avatar
-        self.sign = sign
-    }
-
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decode(UInt.self, forKey: .id)
-        self.username = try container.decode(String.self, forKey: .username)
-        self.nickname = try container.decode(String.self, forKey: .nickname)
-        self.userGroup = try container.decode(UInt.self, forKey: .userGroup)
-        self.avatar = try container.decode(Avatar.self, forKey: .avatar)
-        self.sign = try container.decode(String.self, forKey: .sign)
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.id, forKey: .id)
-        try container.encode(self.username, forKey: .username)
-        try container.encode(self.nickname, forKey: .nickname)
-        try container.encode(self.userGroup, forKey: .userGroup)
-        try container.encode(self.avatar, forKey: .avatar)
-        try container.encode(self.sign, forKey: .sign)
-    }
-}
-
-@Model
-final class Auth {
+struct TokenResponse: Codable {
     var accessToken: String
-    var expiresAt: Date
-    @Attribute(.unique)
+    var expiresIn: UInt
+    var tokenType: String
     var refreshToken: String
+}
 
-    init(accessToken: String, expiresAt: Date, refreshToken: String) {
-        self.accessToken = accessToken
-        self.expiresAt = expiresAt
-        self.refreshToken = refreshToken
+struct Images: Codable {
+    var large: String
+    var common: String
+    var medium: String
+    var small: String
+    var grid: String
+}
+
+struct Avatar: Codable {
+    var large: String
+    var medium: String
+    var small: String
+}
+
+struct Tag: Codable {
+    var name: String
+    var count: UInt
+}
+
+/// 收藏类型
+///
+/// 1: 想看
+/// 2: 看过
+/// 3: 在看
+/// 4: 搁置
+/// 5: 抛弃
+enum CollectionType: UInt8, Codable {
+    case unknown = 0
+    case wish = 1
+    case collect = 2
+    case `do` = 3
+    case onHold = 4
+    case dropped = 5
+
+    init(value: UInt8 = 0) {
+        let tmp = CollectionType(rawValue: value)
+        if let out = tmp {
+            self = out
+            return
+        }
+        self = CollectionType.unknown
     }
 
-    init(response: TokenResponse) {
-        self.accessToken = response.accessToken
-        self.expiresAt = Date().addingTimeInterval(TimeInterval(response.expiresIn))
-        self.refreshToken = response.refreshToken
+    var description: String {
+        switch self {
+        case .unknown:
+            return "未知"
+        case .wish:
+            return "想看"
+        case .collect:
+            return "看过"
+        case .do:
+            return "在看"
+        case .onHold:
+            return "搁置"
+        case .dropped:
+            return "抛弃"
+        }
     }
 
-    func isExpired() -> Bool {
-        return Date() > self.expiresAt
-    }
-
-    func update(response: TokenResponse) {
-        self.accessToken = response.accessToken
-        self.expiresAt = Date().addingTimeInterval(TimeInterval(response.expiresIn))
-        self.refreshToken = response.refreshToken
+    var icon: String {
+        switch self {
+        case .unknown:
+            return "questionmark"
+        case .wish:
+            return "heart"
+        case .collect:
+            return "checkmark"
+        case .do:
+            return "eye"
+        case .onHold:
+            return "clock"
+        case .dropped:
+            return "trash"
+        }
     }
 }
 
-@Model
-final class UserSubjectCollection: Codable {
-    enum CodingKeys: String, CodingKey {
-        case subjectId
-        case subjectType
-        case rate
-        case type
-        case comment
-        case tags
-        case epStatus
-        case volStatus
-        case updatedAt
-        case `private`
-        case subject
+/// 条目类型
+/// 1 为 书籍
+/// 2 为 动画
+/// 3 为 音乐
+/// 4 为 游戏
+/// 6 为 三次元
+///
+/// 没有 5
+enum SubjectType: UInt8, Codable, Identifiable {
+    case unknown = 0
+    case book = 1
+    case anime = 2
+    case music = 3
+    case game = 4
+    case real = 6
+
+    var id: Self {
+        self
     }
 
-    @Attribute(.unique)
-    var subjectId: UInt
-    var subjectType: SubjectType
-    var rate: UInt8
-    var type: CollectionType
-    var comment: String?
-    var tags: [String]
-    var epStatus: UInt
-    var volStatus: UInt
-    var updatedAt: Date
-    var `private`: Bool
-    var subject: SlimSubject?
-
-    init(subjectId: UInt, subjectType: SubjectType, rate: UInt8, type: CollectionType, comment: String? = nil, tags: [String], epStatus: UInt, volStatus: UInt, updatedAt: Date, private: Bool = false, subject: SlimSubject? = nil) {
-        self.subjectId = subjectId
-        self.subjectType = subjectType
-        self.rate = rate
-        self.type = type
-        self.comment = comment
-        self.tags = tags
-        self.epStatus = epStatus
-        self.volStatus = volStatus
-        self.updatedAt = updatedAt
-        self.private = `private`
-        self.subject = subject
-    }
-
-    required init(from decoder: Decoder) throws {
-        let RFC3339DateFormatter = DateFormatter()
-        RFC3339DateFormatter.locale = Locale(identifier: "en_US_POSIX")
-        RFC3339DateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZZZZZ"
-        RFC3339DateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.subjectId = try container.decode(UInt.self, forKey: .subjectId)
-        self.subjectType = try container.decode(SubjectType.self, forKey: .subjectType)
-        self.rate = try container.decode(UInt8.self, forKey: .rate)
-        self.type = try container.decode(CollectionType.self, forKey: .type)
-        self.comment = try container.decode(String?.self, forKey: .comment)
-        self.tags = try container.decode([String].self, forKey: .tags)
-        self.epStatus = try container.decode(UInt.self, forKey: .epStatus)
-        self.volStatus = try container.decode(UInt.self, forKey: .volStatus)
-        guard let updatedAt = try container.decodeIfPresent(String.self, forKey: .updatedAt) else {
-            throw ChiiError(message: "Invalid updatedAt")
+    init(value: UInt8 = 0) {
+        let tmp = SubjectType(rawValue: value)
+        if let out = tmp {
+            self = out
+            return
         }
-        guard let updatedAt = RFC3339DateFormatter.date(from: updatedAt) else {
-            throw ChiiError(message: "Decode updatedAt failed: \(updatedAt)")
-        }
-        self.updatedAt = updatedAt
-        self.private = try container.decode(Bool.self, forKey: .private)
-        self.subject = try container.decode(SlimSubject?.self, forKey: .subject)
+        self = SubjectType.unknown
     }
 
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(self.subjectId, forKey: .subjectId)
-        try container.encode(self.subjectType, forKey: .subjectType)
-        try container.encode(self.rate, forKey: .rate)
-        try container.encode(self.type, forKey: .type)
-        try container.encode(self.comment, forKey: .comment)
-        try container.encode(self.tags, forKey: .tags)
-        try container.encode(self.epStatus, forKey: .epStatus)
-        try container.encode(self.volStatus, forKey: .volStatus)
-        try container.encode(self.updatedAt, forKey: .updatedAt)
-        try container.encode(self.private, forKey: .private)
-        try container.encode(self.subject, forKey: .subject)
+    var description: String {
+        switch self {
+        case .unknown:
+            return "未知"
+        case .book:
+            return "书籍"
+        case .anime:
+            return "动画"
+        case .music:
+            return "音乐"
+        case .game:
+            return "游戏"
+        case .real:
+            return "三次元"
+        }
     }
+
+    var icon: String {
+        switch self {
+        case .unknown:
+            return "questionmark"
+        case .book:
+            return "book"
+        case .anime:
+            return "film"
+        case .music:
+            return "music"
+        case .game:
+            return "gamecontroller"
+        case .real:
+            return "photo"
+        }
+    }
+
+    static func progressTypes() -> [SubjectType] {
+        return [.anime, .book, .real]
+    }
+}
+
+struct CollectionResponse: Codable {
+    var total: UInt
+    var limit: UInt
+    var offset: UInt
+    var data: [UserSubjectCollection]
+}
+
+struct SlimSubject: Codable {
+    var id: UInt
+    var type: SubjectType
+    var name: String
+    var nameCn: String
+    var shortSummary: String
+    var date: String?
+    var images: Images
+    var volumes: UInt
+    var eps: UInt
+    var collectionTotal: UInt
+    var score: Float
+    var tags: [Tag]
+}
+
+struct SearchSubject: Codable {
+    var id: UInt
+    var type: SubjectType?
+    var date: String
+    var image: String
+    var summary: String
+    var name: String
+    var nameCn: String
+    var tags: [Tag]
+    var score: Float
+    var rank: UInt
+}
+
+struct SubjectSearchResponse: Codable {
+    var total: UInt
+    var limit: UInt
+    var offset: UInt
+    var data: [SearchSubject]
 }
