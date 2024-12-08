a604957cdf0f57b2fd65021b58aeed7fc6a2c6b7
everpcpc <git@everpcpc.com>
2024-12-08 23:40:14 +0800

    fix: subject relation view

--- Chobits/Views/Subject/SubjectRelationsView.swift
+++ Chobits/Views/Subject/SubjectRelationsView.swift
@@ -19,6 +19,7 @@ struct SubjectRelationsView: View {
   @State private var loading: Bool = false
   @State private var relations: [SubjectRelationDTO] = []
   @State private var offprints: [SubjectRelationDTO] = []
+  @State private var collections: [Int: CollectionType] = [:]
 
   @Query private var subjects: [Subject]
   var subject: Subject? { subjects.first }
@@ -35,6 +36,18 @@ struct SubjectRelationsView: View {
         offprints.append(contentsOf: offprintResp.data)
         let relationResp = try await Chii.shared.getSubjectRelations(subjectId, limit: 10)
         relations.append(contentsOf: relationResp.data)
+
+        var relationIds: [Int] = []
+        relationIds.append(contentsOf: relations.map { $0.subject.id })
+        relationIds.append(contentsOf: offprints.map { $0.subject.id })
+        let collectionDescriptor = FetchDescriptor<UserSubjectCollection>(
+          predicate: #Predicate<UserSubjectCollection> {
+            relationIds.contains($0.subjectId)
+          })
+        let collects = try modelContext.fetch(collectionDescriptor)
+        for collection in collects {
+          self.collections[collection.subjectId] = collection.typeEnum
+        }
       } catch {
         Notifier.shared.alert(error: error)
       }
@@ -57,9 +70,22 @@ struct SubjectRelationsView: View {
           ForEach(offprints) { offprint in
             NavigationLink(value: NavDestination.subject(subjectId: offprint.subject.id)) {
               VStack {
-                ImageView(
-                  img: offprint.subject.images?.common,
-                  width: 60, height: 80, type: .subject)
+                if let ctype = collections[offprint.subject.id] {
+                  ImageView(
+                    img: offprint.subject.images?.common,
+                    width: 60, height: 80, type: .subject, overlay: .caption
+                  ) {
+                    HStack {
+                      Image(systemName: ctype.icon)
+                      Spacer()
+                      Text(ctype.description(type: offprint.subject.type))
+                    }.padding(.horizontal, 4)
+                  }
+                } else {
+                  ImageView(
+                    img: offprint.subject.images?.common,
+                    width: 60, height: 80, type: .subject)
+                }
                 Spacer()
               }
               .font(.caption)
@@ -91,21 +117,33 @@ struct SubjectRelationsView: View {
         ForEach(relations) { relation in
           NavigationLink(value: NavDestination.subject(subjectId: relation.subject.id)) {
             VStack {
-              ImageView(
-                img: relation.subject.images?.common,
-                width: 90, height: 120,
-                type: .subject, overlay: .caption
-              ) {
-                VStack {
-                  if relation.relation.cn.isEmpty {
-                    Text(relation.subject.type.description)
-                  } else {
-                    Text(relation.relation.cn)
-                  }
+              Section {
+                if relation.relation.cn.isEmpty {
+                  Text(relation.subject.type.description)
+                } else {
+                  Text(relation.relation.cn)
                 }
-                .lineLimit(1)
-                .font(.caption)
-                .foregroundStyle(.white)
+              }
+              .lineLimit(1)
+              .font(.caption)
+
+              if let ctype = collections[relation.subject.id] {
+                ImageView(
+                  img: relation.subject.images?.common,
+                  width: 90, height: 120,
+                  type: .subject, overlay: .caption
+                ) {
+                  HStack {
+                    Image(systemName: ctype.icon)
+                    Spacer()
+                    Text(ctype.description(type: relation.subject.type))
+                  }.padding(.horizontal, 4)
+                }
+              } else {
+                ImageView(
+                  img: relation.subject.images?.common,
+                  width: 90, height: 120, type: .subject
+                )
               }
               Text(relation.subject.name)
                 .font(.caption)
@@ -113,7 +151,7 @@ struct SubjectRelationsView: View {
                 .truncationMode(.middle)
                 .lineLimit(2)
               Spacer()
-            }.frame(width: 90, height: 165)
+            }.frame(width: 90, height: 185)
           }.buttonStyle(.plain)
         }
       }
