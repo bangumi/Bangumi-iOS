a574e327690bcd72a8408d94a5bff36c6e71e0d9
everpcpc <git@everpcpc.com>
2025-02-16 21:51:46 +0800

    feat: add episode recent slim view

--- Chobits/Views/Episode/EpisodeRecentView.swift
+++ Chobits/Views/Episode/EpisodeRecentView.swift
@@ -159,7 +159,80 @@ struct EpisodeRecentView: View {
           }.buttonStyle(.plain)
         }
       }
+    }.animation(.default, value: episodes)
+  }
+}
+
+struct EpisodeRecentSlimView: View {
+  let subjectId: Int
+
+  @Environment(Subject.self) var subject
+  @Environment(\.modelContext) var modelContext
+
+  @State private var updating: Bool = false
+
+  @Query private var pendingEpisodes: [Episode]
+  private var nextEpisode: Episode? { pendingEpisodes.first }
+
+  init(subjectId: Int) {
+    self.subjectId = subjectId
+    var descriptor = FetchDescriptor<Episode>(
+      predicate: #Predicate<Episode> {
+        $0.subjectId == subjectId && $0.type == 0 && $0.status == 0
+      }, sortBy: [SortDescriptor<Episode>(\.sort, order: .forward)])
+    descriptor.fetchLimit = 1
+    _pendingEpisodes = Query(descriptor)
+  }
+
+  func updateSingle(episode: Episode, type: EpisodeCollectionType) {
+    if updating { return }
+    updating = true
+    Task {
+      do {
+        try await Chii.shared.updateEpisodeCollection(
+          episodeId: episode.episodeId, type: type)
+        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
+      } catch {
+        Notifier.shared.alert(error: error)
+      }
+      updating = false
     }
-    .animation(.default, value: episodes)
+  }
+
+  var body: some View {
+    HStack {
+      Text("\(subject.interest?.epStatus ?? 0) / \(subject.eps)")
+        .foregroundStyle(.secondary)
+      Spacer()
+      if let episode = nextEpisode {
+        if episode.air > Date() {
+          Text("EP.\(episode.sort.episodeDisplay) ~ \(episode.waitDesc)")
+            .foregroundStyle(.secondary)
+        } else {
+          if updating {
+            ZStack {
+              Button("EP... 看过", action: {})
+                .disabled(true)
+                .hidden()
+              ProgressView()
+            }
+          } else {
+            Button {
+              updateSingle(episode: episode, type: .collect)
+            } label: {
+              Label(
+                "EP.\(episode.sort.episodeDisplay) 看过",
+                systemImage: "checkmark.circle"
+              )
+            }
+          }
+        }
+      } else {
+        NavigationLink(value: NavDestination.subject(subjectId)) {
+          Image(systemName: "square.grid.2x2.fill")
+            .foregroundStyle(.secondary)
+        }.buttonStyle(.plain)
+      }
+    }.font(.callout)
   }
 }
--- Chobits/Views/Progress/ProgressListView.swift
+++ Chobits/Views/Progress/ProgressListView.swift
@@ -52,42 +52,6 @@ struct ProgressListItemView: View {
 
   @Environment(Subject.self) var subject
 
-  @Environment(\.modelContext) var modelContext
-
-  @State private var updating: Bool = false
-
-  @Query private var pendingEpisodes: [Episode]
-  private var nextEpisode: Episode? { pendingEpisodes.first }
-
-  init(subjectId: Int) {
-    self.subjectId = subjectId
-    var descriptor = FetchDescriptor<Episode>(
-      predicate: #Predicate<Episode> {
-        $0.subjectId == subjectId && $0.type == 0 && $0.status == 0
-      }, sortBy: [SortDescriptor<Episode>(\.sort, order: .forward)])
-    descriptor.fetchLimit = 1
-    _pendingEpisodes = Query(descriptor)
-  }
-
-  func markNextWatched() {
-    guard let episodeId = nextEpisode?.episodeId else {
-      return
-    }
-    if updating {
-      return
-    }
-    updating = true
-    Task {
-      do {
-        try await Chii.shared.updateEpisodeCollection(episodeId: episodeId, type: .collect)
-        UIImpactFeedbackGenerator(style: .medium).impactOccurred()
-      } catch {
-        Notifier.shared.alert(error: error)
-      }
-      updating = false
-    }
-  }
-
   var body: some View {
     VStack(alignment: .leading, spacing: 4) {
       HStack {
@@ -115,33 +79,9 @@ struct ProgressListItemView: View {
 
           switch subject.typeEnum {
           case .anime, .real:
-            HStack {
-              Text("\(subject.interest?.epStatus ?? 0) / \(subject.eps)")
-                .foregroundStyle(.secondary)
-              Spacer()
-              if let episode = nextEpisode {
-                if episode.air > Date() {
-                  Text("EP.\(episode.sort.episodeDisplay) ~ \(episode.waitDesc)")
-                    .foregroundStyle(.secondary)
-                } else {
-                  if updating {
-                    ZStack {
-                      Button("EP... 看过", action: {})
-                        .disabled(true)
-                        .hidden()
-                      ProgressView()
-                    }
-                  } else {
-                    Button("EP.\(episode.sort.episodeDisplay) 看过", action: markNextWatched)
-                  }
-                }
-              } else {
-                NavigationLink(value: NavDestination.subject(subjectId)) {
-                  Image(systemName: "square.grid.2x2.fill")
-                    .foregroundStyle(.secondary)
-                }.buttonStyle(.plain)
-              }
-            }.font(.callout)
+            EpisodeRecentSlimView(subjectId: subjectId)
+              .environment(subject)
+
           case .book:
             SubjectBookChaptersView(mode: .row)
               .environment(subject)
