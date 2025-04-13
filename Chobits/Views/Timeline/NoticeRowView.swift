4ea6ec48d683f12c2a04b548bbc1b739ea153194
everpcpc <git@everpcpc.com>
2025-04-13 13:56:52 +0800

    fix: adjust notice row

--- Chobits/Views/Timeline/NoticeRowView.swift
+++ Chobits/Views/Timeline/NoticeRowView.swift
@@ -7,32 +7,32 @@ struct NoticeRowView: View {
   @Binding var notice: NoticeDTO
 
   var statusColor: Color {
-    notice.unread ? .accent : .secondary.opacity(0.5)
+    notice.unread ? .accent : .secondary.opacity(0.2)
   }
 
   var body: some View {
     HStack(alignment: .top) {
-      Rectangle()
-        .frame(width: 4)
-        .foregroundStyle(statusColor)
       ImageView(img: notice.sender.avatar?.large)
         .imageStyle(width: 40, height: 40)
         .imageType(.avatar)
         .imageLink(notice.sender.link)
-      VStack(alignment: .leading) {
-        HStack {
-          Text(notice.sender.nickname.withLink(notice.sender.link))
-          Spacer()
-          Text(notice.createdAt.datetimeDisplay)
-            .font(.footnote)
-            .foregroundStyle(.secondary)
-            .lineLimit(1)
+      HStack(alignment: .top) {
+        Rectangle()
+          .frame(width: 4)
+          .foregroundStyle(statusColor)
+        VStack(alignment: .leading) {
+          HStack {
+            Text(notice.sender.nickname.withLink(notice.sender.link))
+            Spacer()
+            Text(notice.createdAt.datetimeDisplay)
+              .font(.footnote)
+              .foregroundStyle(.secondary)
+              .lineLimit(1)
+          }
+          Text(notice.desc)
         }
-        Text(notice.desc)
       }
-      Spacer()
-    }
-    .animation(.default, value: notice)
+    }.animation(.default, value: notice)
     Divider()
   }
 }
